a7e833859cf70b7580fa1dc8e884990dcca6b13e
Takemi <takemi.9814+github@gmail.com>
2025-06-01 10:55:08 +0900

    init

---
+++ ConsoleApp/Application.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+  internal class Application
+  {
+    private const int EXIT_SUCCESS = 0;
+    private const int EXIT_FAILURE = 1;
+
+    public int Run(string[] args)
+    {
+      return EXIT_SUCCESS;
+    }
+  }
+}
